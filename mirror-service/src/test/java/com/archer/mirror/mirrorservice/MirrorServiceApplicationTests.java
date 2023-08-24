9cc1bc674561358a742da24f1b0b3772bbb9d74d
ArNine <851332120@qq.com>
2023-08-24 22:41:04 +0800

    [Feature]project init

---
+++ mirror-service/src/test/java/com/archer/mirror/mirrorservice/MirrorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.archer.mirror.mirrorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MirrorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
