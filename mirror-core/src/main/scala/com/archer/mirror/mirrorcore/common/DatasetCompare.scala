ff13f73a36aaae82ae07cf328470f489b6c6f696
ArNine <851332120@qq.com>
2023-10-10 00:06:34 +0800

    [Feature]create compare file

---
+++ mirror-core/src/main/scala/com/archer/mirror/mirrorcore/common/DatasetCompare.scala
@@ -0,0 +1,10 @@
+package com.archer.mirror.mirrorcore.common
+
+import com.archer.mirror.mirrorcore.source.DataSource
+
+class DatasetCompare(base: DataSource, compare: DataSource) {
+
+
+
+
+}
--- mirror-core/src/main/scala/com/archer/mirror/mirrorcore/source/FileSource.scala
+++ mirror-core/src/main/scala/com/archer/mirror/mirrorcore/source/FileSource.scala
@@ -4,16 +4,16 @@ import com.archer.mirror.mirrorcore.common.MirrorContext
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.{Column, DataFrame}
 
-class FileSource(filePath: String) extends DataSource {
+class FileSource(fileType: String, filePath: String) extends DataSource {
 
   private val dataset = readFile(filePath)
   private var primary: Seq[Column] = _
   private def readFile(filePath: String) = MirrorContext.getOrCreateSpark().read
-    .format("json").option("header", "true").load(filePath)
+    .format(fileType).option("header", "true").load(filePath)
 
 
-  def this(filePath: String, primary: Seq[String]) = {
-    this(filePath)
+  def this(fileType: String, filePath: String, primary: Seq[String]) = {
+    this(fileType, filePath)
     val spark = MirrorContext.getOrCreateSpark()
     import spark.implicits._
     this.primary = primary.map(x => {
